eb3c45cdaf4d5de683bdd5c6a5ebe948143b3c87
nepp.plus <nepp.plus@gmail.com>
2022-02-08 14:56:24 +0900

    ORM의 필터 기능 활용

--- phonebook/api/user/user.py
+++ phonebook/api/user/user.py
@@ -1,13 +1,22 @@
 from django.views import View
 from django.http import JsonResponse
 
+from phonebook.models import Users
+
 class User(View):
     
-    # get으로 접근
+    # get으로 접근 => 사용자 목록 보기 (임시)
     def get(self, request):
+        
+        db_user = Users.objects.filter(email='cho881020@gmail.com').first()
+        
+        # users = [ user.get_data_object() for user in db_users ]
+        
         return JsonResponse( {
             'code': 200,
-            'message': '임시 - GET 테스트'
+            'message': '임시 - GET 테스트',
+            # 'users': users
+            'user' : db_user.get_data_object()
         } )
         
     
