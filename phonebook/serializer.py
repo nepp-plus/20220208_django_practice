d048a7e17f1d369fad0ed5f1093824bd2a8c8ad5
nepp.plus <nepp.plus@gmail.com>
2022-02-08 16:42:02 +0900

    Users 클래스를 직렬화 해주는 클래스 추가

--- phonebook/serializer.py
+++ phonebook/serializer.py
@@ -2,3 +2,12 @@
 # 각각의 모델 클래스들을 > dict / list로 변경해주는 기능을 구현.
 
 # rest_framework 모듈의 도움을 받자.
+from rest_framework import serializers
+# 어떤 모델을 직렬화 (ORM -> Dict) 할건지, 모델을 끌어오자.
+from .models import Users
+
+# 실제 사용자 모델을 변환해주는 클래스
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Users  # 어떤 클래스를 변환?
+        fields = '__all__' # 모든 컬럼을 dict에 담자.
