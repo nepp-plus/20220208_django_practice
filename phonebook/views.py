6bb219bb9c9491b5654fe4fcdd16add186b2e6e0
nepp.plus <nepp.plus@gmail.com>
2022-02-08 12:49:25 +0900

    JSON 응답 예시

--- phonebook/views.py
+++ phonebook/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 
 # Create your views here.
 
 def test_home(request):
     content = '<h1>여기는 홈 화면 입니다.</h1>' # HTML태그를 리턴.
     return HttpResponse(content)
+
+def json_test(request):
+    my_dict = {
+        'code': 200,
+        'message': 'JSON이 내려오는지 테스트'
+    }
+    
+    return JsonResponse(my_dict)
