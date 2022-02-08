5c7ed47ad8436c19e13a9ea2ef45b82247343957
nepp.plus <nepp.plus@gmail.com>
2022-02-08 16:33:18 +0900

    새 DB 모델 코드 적용
     - dict로 변경해주는 함수 증발.

--- phonebook/models.py
+++ phonebook/models.py
@@ -8,18 +8,6 @@ class Contacts(models.Model):
     memo = models.TextField()
     created_at = models.DateTimeField()
 
-    def get_data_object(self):
-        data = {
-            'id': self.id,
-            'name': self.name,
-            'phone_num': self.phone_num,
-            'email': self.email,
-            'memo': self.memo,
-            'created_at': str(self.created_at),
-            'user': self.user.get_data_object(),
-        }
-        return data
-
     class Meta:
         managed = False
         db_table = 'contacts'
@@ -30,16 +18,6 @@ class Users(models.Model):
     password = models.CharField(max_length=50)
     nickname = models.CharField(max_length=50)
     created_at = models.DateTimeField()
-
-    # DB 수행 결과 > dict로 변경 메쏘드
-    
-    def get_data_object(self):
-        data = {
-            'email': self.email,
-            'nickname': self.nickname,
-            'created_at': str(self.created_at)
-        }
-        return data
 
     class Meta:
         managed = False
