f159a0c9b583fed2a0129595fc5ff563c50df99c
nepp.plus <nepp.plus@gmail.com>
2022-02-08 15:11:09 +0900

    ORM 필터 연습

--- phonebook/api/contact/contact.py
+++ phonebook/api/contact/contact.py
@@ -1,13 +1,20 @@
 from django.views import View
 from django.http import JsonResponse
 
+from phonebook.models import Contacts
 
 class Contact(View):
     
     def get(self, request):
+        
+        db_contact = Contacts.objects.filter(name='조경진').first()
+        
         return JsonResponse({
             'code': 200,
-            'message': '연락처 - GET 테스트'
+            'message': '연락처 - GET 테스트',
+            'data': {
+                'contact': db_contact.get_data_object()
+            }
         })
         
     def post(self, request):
