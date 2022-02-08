f08a398893cca043336fac19c2baa12a2e5fce22
nepp.plus <nepp.plus@gmail.com>
2022-02-08 12:40:08 +0900

    기본적인 http 응답 내려주기

--- phonebook/views.py
+++ phonebook/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def test_home(request):
+    content = '<h1>여기는 홈 화면 입니다.</h1>' # HTML태그를 리턴.
+    return HttpResponse(content)
